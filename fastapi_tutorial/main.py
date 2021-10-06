800d06c053b0b47e64e05edbc594f028a32b380c
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-06 07:03:11 -0500

    Creating blog

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -1,16 +1,19 @@
 from typing import Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from . import models, schemas
-from .database import engine
+from .database import engine, SessionLocal
 from .models import Blog
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
-#@app.get('/blog')
-#def index(limit=10, published: bool = False, sort: Optional[str] = None):
+
+
+# @app.get('/blog')
+# def index(limit=10, published: bool = False, sort: Optional[str] = None):
 #    if published:
 #        data = {'message': f'{limit} published blogs from db', 'name': 'kilo'}
 #    else:
@@ -18,26 +21,34 @@ models.Base.metadata.create_all(engine)
 #    return data
 #
 #
-#@app.get('/blog/unpublished')
-#def unpublished():
+# @app.get('/blog/unpublished')
+# def unpublished():
 #    data = {'message': 'All unpublished blogs', }
 #    return data
 #
 #
-#@app.get('/blog/{blog_id}')
-#def show(blog_id: int):
+# @app.get('/blog/{blog_id}')
+# def show(blog_id: int):
 #    data = {'message': 'About data', 'id': blog_id}
 #    return data
 #
 #
-#@app.get('/blog/{blog_id}/comments')
-#def comments(blog_id: int):
+# @app.get('/blog/{blog_id}/comments')
+# def comments(blog_id: int):
 #    data = {'message': ['Comment 1', ['Comment2']], 'id': blog_id}
 #    return data
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.post('/blog/create')
-def create_blog(request: schemas.Blog):
-    return request
-    data = {'message': 'Blog is created'}
-    return data
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
