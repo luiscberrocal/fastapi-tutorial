012e4ba3284ac3b2aa89b4e678cad0b1d73b6070
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-07 07:19:02 -0500

    Half baked solution. Update not working.

--- fastapi_tutorial/main.py
+++ fastapi_tutorial/main.py
@@ -56,8 +56,12 @@ def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(request,
-                                                              synchronize_session=False)  # noqa
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} onot found')
+
+    blog.update(request)  # noqa
     db.commit()
     return 'Updated'
 
